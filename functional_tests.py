1dc2f2c2c3fe23f654174ed22fe3a253e6303170
Vadim Prokoshin <vpro85@gmail.com>
2021-10-29 18:56:02 +0200

    before refactoring

--- functional_tests.py
+++ functional_tests.py
@@ -47,21 +47,26 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Купить павлиньи перья' for row in rows),
-            "Новый элемент списка не появился в таблице"
-        )
+        self.assertIn('1: Купить павлиньи перья', [row.text for row in rows])
 
         # Текстовое поле по-прежнему приглашает ее добавить еще один элемент.
         # Она вводит "Сделать мушку из павлиньих перьев" в качестве элемента списка
         # (Эдит очень методична)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Сделать мушку из павлиньих перьев')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # Страница снова обновляется, и теперь показывает оба элемента ее списка
-        self.fail('Закончить!')
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Купить павлиньи перья', [row.text for row in rows])
+        self.assertIn('2: Сделать мушку из павлиньих перьев', [row.text for row in rows])
 
         # Эдит интересно, запомнит ли сайт ее список. Далее она видит, что
         # сайт сгенерировал для нее уникальный url-адрес - об этом
         # выводится небольшой текст с объяснениями.
+        self.fail('Закончить!')
 
         # Она посещает этот url-адрес - ее список по-прежнему там.
 
--- lists/tests.py
+++ lists/tests.py
@@ -4,7 +4,13 @@ from django.test import TestCase
 class HomePageTest(TestCase):
     """ тест домашней страницы """
 
-    def test_home_page_returns_correct_html(self):
+    def test_uses_home_template(self):
         """ тест: домашняя страница возвращает правильный html"""
         response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        """тест: можно сохранить post-запрос"""
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
