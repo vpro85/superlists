65e29b71326136d163eb93e9786ced8f4a38edf4
Vadim Prokoshin <vpro85@gmail.com>
2021-10-29 12:53:53 +0200

    First module test and url mapping, dummy view

--- lists/tests.py
+++ lists/tests.py
@@ -1,9 +1,13 @@
+from django.urls import resolve
 from django.test import TestCase
+from lists.views import home_page
 
 
-class SmokeTest(TestCase):
-    """ тест на токсичность """
+class HomePageTest(TestCase):
+    """ тест домашней страницы """
 
-    def test_bad_maths(self):
-        """ тест: неправильные математические расчеты """
-        self.assertEqual(1 + 1, 3)
+    def test_root_url_resolves_to_home_page_view(self):
+        """ тест: корневой url преобразуется в представление
+        домашней страницы """
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
