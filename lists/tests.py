022404c386922f5d58fc0a167465a3fbf29a38f0
Vadim Prokoshin <vpro85@gmail.com>
2021-11-02 18:32:35 +0200

    Add view_list and tests

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,25 @@ from django.test import TestCase
 from lists.models import Item
 
 
+class ListViewTest(TestCase):
+    """тест представления списка"""
+
+    def test_uses_list_template(self):
+        """тест: используется шаблон списка"""
+        response = self.client.get('/lists/only-one-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
+
+    def test_displays_all_items(self):
+        """тест: отображаются все элементы списка"""
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/lists/only-one-list-in-the-world/')
+
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
+
+
 class ItemModelTest(TestCase):
     """тест модели элемента списка"""
 
@@ -44,18 +63,9 @@ class HomePageTest(TestCase):
         """тест: переадресует после post-запроса"""
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'], '/lists/only-one-list-in-the-world/')
 
     def test_only_saves_items_when_necessary(self):
         """тест: сохраняет элементы только когда нужно"""
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
-
-    def test_displays_all_list_items(self):
-        """тест: отображаются все элементы списка"""
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
-
-        response = self.client.get('/')
-        self.assertIn('itemey 1', response.content.decode())
-        self.assertIn('itemey 2', response.content.decode())
