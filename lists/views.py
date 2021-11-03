6c15d79b91f10821d989462a4f0b3c4ea3c6fbd5
Vadim Prokoshin <vpro85@gmail.com>
2021-11-03 10:34:40 +0200

    Скорректировать модель, чтобы элементы были связаны с разными списками

--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,14 @@
 import ipdb
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from lists.models import Item
+from lists.models import Item, List
+
+
+def new_list(request):
+    """новый список"""
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect('/lists/only-one-list-in-the-world/')
 
 
 def home_page(request):
@@ -13,10 +20,4 @@ def view_list(request):
     """представление списка"""
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
-
-
-def new_list(request):
-    """новый список"""
-    Item.objects.create(text=request.POST['item_text'])
-    return redirect('/lists/only-one-list-in-the-world/')
 
